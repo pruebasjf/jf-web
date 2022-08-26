1b9e7e8902808b6f424dcc0fc8a45c811b08b784
pruebasjf <110250076+pruebasjf@users.noreply.github.com>
2022-08-26 11:35:24 -0300

    Delete js directory

--- js/enviado.js
+++
@@ -1,5 +0,0 @@
-import {copyRigth} from "../js/app.js"
-
-copyRigth()
-
-setTimeout(()=>{window.location.href= "https://pruebasjf.github.io/jf-web"}, 7000);
--- js/posventa.js
+++
@@ -1,3 +0,0 @@
-import { copyRigth } from "../js/app.js";
-
-copyRigth()
